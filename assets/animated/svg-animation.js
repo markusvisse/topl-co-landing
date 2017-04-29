a4a807ae5e0568546a3c63746ee9f6a29ba07655
Zihe Huang <zihehuang@gmail.com>
2017-04-29 02:51:43 -0500

    need to design hub icon

--- assets/animated/svg-animation.js
+++ assets/animated/svg-animation.js
@@ -13,8 +13,8 @@ $(window).load(function() {
     function calcLength ($el) {
         return $el[0].getTotalLength()
     }
-	var $line1 = $("#Line1");
-	pathPrepare($line1);
+	var $line1 = $("#Line1"); var $line2 = $("#Line2");
+	pathPrepare($line1); //pathPrepare($line2);
 
     var controller = new ScrollMagic.Controller();
 	
@@ -23,35 +23,52 @@ $(window).load(function() {
         triggerElement: "#block", offset: 400
     }).setTween(tween).addIndicators({name: "1 - Draw Line 1"}).addTo(controller);
 		
-    new ScrollMagic.Scene({
+	new ScrollMagic.Scene({
         triggerElement: "#block", offset: 500
-    }).setClassToggle("#BlockGroup1", "appear").addIndicators({name: "2 - Make BlockGroup1 Appear"}).addTo(controller);
+    }).setTween(TweenMax.to("#BlockGroup1", 0.5, {autoAlpha: 1, display: "block"})).addIndicators({name: "2 - Make BlockGroup1 Appear"}).addTo(controller);
 	
-	var tween1 = TweenMax.to($line1, 0.5, {autoAlpha: 0})
 	new ScrollMagic.Scene({
         triggerElement: "#block", offset: 600
-    }).setTween(tween1).addIndicators({name: "3 - Make Line 1 Disappear"}).addTo(controller);
-
-    var producerScene = new ScrollMagic.Scene({
-        triggerElement: "#block", offset: 1000
-    })
-	.setClassToggle("#Group3", "zap1")
-	.addIndicators({name: "3 - add a class"}) // add indicators (requires plugin)
-	.addTo(controller);
+    }).setTween(TweenMax.to($line1, 0.5, {autoAlpha: 0, display: "none"})).addIndicators({name: "3 - Make Line 1 Disappear"}).addTo(controller);
+	new ScrollMagic.Scene({
+        triggerElement: "#block", offset: 600
+    }).setTween(TweenMax.to("#Investor", 0.5, {autoAlpha: 1, display: "block"})).addIndicators({name: "4 - Make Line 1 Disappear"}).addTo(controller);
+	
+	new ScrollMagic.Scene({
+        triggerElement: "#block", offset: 700
+    }).setTween(TweenMax.to($line2, 0.5, {autoAlpha: 1, display: "block"})).addIndicators({name: "5 - Make Line 2 Appear"}).addTo(controller);
+	
+	new ScrollMagic.Scene({
+        triggerElement: "#block", offset: 700
+    }).setTween(TweenMax.to("#LetterOfEngagement", 0.5, {autoAlpha: 1, display: "block"})).addIndicators({name: "6 - Make LetterOfEngagement Appear"}).addTo(controller);
+	
+	new ScrollMagic.Scene({
+        triggerElement: "#block", offset: 800
+    }).setTween(TweenMax.to("#LetterOfEngagement", 0.5, {autoAlpha: 0, display: "none"})).addIndicators({name: "7 - Make LetterOfEngagement Disappear"}).addTo(controller);
+	
+	new ScrollMagic.Scene({
+        triggerElement: "#block", offset: 800
+    }).setTween(TweenMax.to("#NegotiateTerms", 0.5, {autoAlpha: 1, display: "block"})).addIndicators({name: "8 - Make NegotiateTerms Appear"}).addTo(controller);
+	
+	new ScrollMagic.Scene({
+        triggerElement: "#block", offset: 1200
+    }).setTween(TweenMax.to("#NegotiateTerms", 0.5, {autoAlpha: 0, display: "none"})).addIndicators({name: "9 - Make NegotiateTerms Disappear"}).addTo(controller);
+	
+	new ScrollMagic.Scene({
+        triggerElement: "#block", offset: 1200, duration: 200
+    }).setTween(TweenMax.to("#Line3", 0.5, {autoAlpha: 1, display: "block"})).addIndicators({name: "10 - Make Line3 Appear"}).addTo(controller);
+	
+	new ScrollMagic.Scene({
+        triggerElement: "#block", offset: 1200, duration: 300
+    }).setTween(TweenMax.to("#Hub", 0.5, {autoAlpha: 1.5, display: "block"})).addIndicators({name: "11 - Make Hub Appear"}).addTo(controller);
 
     var producerScene = new ScrollMagic.Scene({
         triggerElement: "#block", offset: 1500
     })
-	.setClassToggle("#Group4", "zap1")
-	.addIndicators({name: "4 - add a class"}) // add indicators (requires plugin)
+	.setClassToggle("#BlockGroup2", "zap")
+	.addIndicators({name: "3 - add a class"})
 	.addTo(controller);
 
-    var agreementScene = new ScrollMagic.Scene({
-        triggerElement: "#block", offset: 1500
-    })
-    .setClassToggle("#upper0", "zap")
-    .addIndicators({name: "5 - Remove Upper Zero"}) // add indicators (requires plugin)
-    .addTo(controller);
     // var producerSceneDelete = new ScrollMagic.Scene({
     // 	triggerElement: "#block", offset: 1000
     // })
