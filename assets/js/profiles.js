ecb244a4df625d7bd28a6df1040772fbcd3f4fee
MarkusVisse <ankorki@gmail.com>
2018-05-30 23:56:57 +0200

    Updated team
    Updated team and advisors

--- assets/js/profiles.js
+++ assets/js/profiles.js
@@ -1,36 +1,36 @@
 function profileOnClick(img, name) {
-  
+
   var x = document.querySelector('#' + name + '.profileBio');
   var newimg = document.getElementById(img);
-  
-  if(x.style.display === 'none') {
+
+  if (x.style.display === 'none') {
     x.style.display = 'flex';
   } else {
     x.style.display = 'none';
   }
-  
-  if(newimg.style.borderBottom !== null && newimg.style.borderBottom !== 'none' && newimg.style.borderBottom !== '') {
+
+  if (newimg.style.borderBottom !== null && newimg.style.borderBottom !== 'none' && newimg.style.borderBottom !== '') {
     newimg.style.borderBottom = 'none';
   } else {
     newimg.style.borderBottom = '8px solid #00A18E';
     newimg.style.paddingBottom = '5px';
   }
-  
+
   var openProfiles = document.getElementsByClassName('profileBio');
-  
-  for(var i = 0; i < openProfiles.length; i++) {
-    if((openProfiles[i].style.display === 'flex' || openProfiles[i].style.display === null) && openProfiles[i].id !== name) {
+
+  for (var i = 0; i < openProfiles.length; i++) {
+    if ((openProfiles[i].style.display === 'flex' || openProfiles[i].style.display === null) && openProfiles[i].id !== name) {
       openProfiles[i].style.display = 'none';
     }
   }
 
-  var openBorders = ['chris-img', 'jim-img', 'nick-img', 'kim-img', 'yamir-img', 'albert-img', 'alex-img', 'matt-img', 'paige-img'];
+  var openBorders = ['chris-img', 'jim-img', 'nick-img', 'kim-img', 'yamir-img', 'albert-img', 'alex-img', 'matt-img', 'paige-img', 'oskar-img', 'anna-img', 'thomas-img', 'mitchell-img', 'gary-img'];
   openBorders.splice(openBorders.indexOf(img), 1);
-  
-  for(var i = 0; i < openBorders.length; i++) {
+
+  for (var i = 0; i < openBorders.length; i++) {
     var border = document.getElementById(openBorders[i]);
-    if(border.style.borderBottom !== null) {
-      if(border.style.borderBottom !== 'none') {
+    if (border.style.borderBottom !== null) {
+      if (border.style.borderBottom !== 'none') {
         border.style.borderBottom = 'none';
       }
     }
